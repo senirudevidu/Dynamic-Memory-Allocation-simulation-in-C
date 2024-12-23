0e0808645d9fd4a221e82f441ca0eca528ad739d
Seniru Seneweera <152300004+senirudevidu@users.noreply.github.com>
2024-12-23 12:51:07 +0530

    add example test case to main.c

--- main.c
+++ main.c
@@ -4,22 +4,18 @@
 
 int main(){
     initialize();
+
+    // You can write some test cases to test this simulation and understand well
+   
+    /*
+    // This is an example test case for demostration
     void* ptr1 = MyMalloc(100*sizeof(int));
     if(ptr1 != NULL){
         printf("Memory allocated at %p\n", ptr1);
     }
-    void* ptr2 = MyMalloc(200*sizeof(char));
-    if(ptr2 != NULL){
-        printf("Memory allocated at %p\n", ptr2);
-    }
-    void* ptr3 = MyMalloc(300*sizeof(float));
-    if(ptr3 != NULL){
-        printf("Memory allocated at %p\n", ptr3);
-    }
 
     MyFree(ptr1);
-    MyFree(ptr2);
-    MyFree(ptr3);
+    */
 
     return 0;
 }
